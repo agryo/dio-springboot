9bd9f78e13fa42351953bbcc56dac5886b4a0d6b
Agryo Araujo <agryostallion@gmail.com>
2023-04-07 15:34:08 -0300

    Delete Site directory
    Erro

--- Site/wp-content/jetpack-waf/rules/rules.php
+++
@@ -1,4 +0,0 @@
-<?php
-if ( file_exists( '/var/www/html/wp-content/jetpack-waf/rules/allow-ip.php' ) ) { if ( require( '/var/www/html/wp-content/jetpack-waf/rules/allow-ip.php' ) ) { return; } }
-if ( file_exists( '/var/www/html/wp-content/jetpack-waf/rules/block-ip.php' ) ) { if ( require( '/var/www/html/wp-content/jetpack-waf/rules/block-ip.php' ) ) { return $waf->block('block', -1, 'ip block list'); } }
-
