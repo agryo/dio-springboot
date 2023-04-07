035bd0e83196eda72ed3e12f2d048ece87e318c4
Agryo Araujo <agryostallion@gmail.com>
2023-04-07 16:45:24 -0300

    Mudança

--- GitHub/primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
+++ GitHub/primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -1,11 +1,21 @@
 package dio.springboot;
 
+import dio.springboot.app.ConversorJson;
+import dio.springboot.app.ViaCepResponse;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrimeirosPassosApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(PrimeirosPassosApplication.class, args);
+	public static void main(String[] args) { SpringApplication.run(PrimeirosPassosApplication.class, args);	}
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) throws Exception {
+		return args -> {
+			String json = "{\"cep\": \"01001-000\",\"logradouro\": \"Praça João de Gois\",\"localidade\"Cruzeta\"}";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do CEP: " + response);
+		};
 	}
 }
